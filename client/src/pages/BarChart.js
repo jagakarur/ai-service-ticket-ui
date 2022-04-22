ae872d2bdec7ef4ac8ef693c85cfe727e5d515d1
jagadheesan <jagakarur@gmail.com>
2022-04-21 23:20:59 -0400

    final chart verstion

--- client/src/pages/BarChart.js
+++ client/src/pages/BarChart.js
@@ -1,81 +1,68 @@
 // BarChart.js
 import * as d3 from 'd3';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
-function BarChart({ width, height, data }) {
+function BarChart({ data }) {
 
 
     useEffect(() => {
         // set the dimensions of the canvas
-        var margin = { top: 20, right: 20, bottom: 70, left: 40 },
-            width = 600 - margin.left - margin.right,
-            height = 300 - margin.top - margin.bottom;
-         var color = d3.scaleOrdinal().range(["#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-        let x = d3.scaleBand()
-                .range([0, width])
-                .padding(0.1)
-
-        let y = d3.scaleLinear()
-                .range([height,0])
-
-        // Generate the x and y Axis based on these scales
-        let xAxis = d3.axisBottom(x)
-        let yAxis = d3.axisLeft(y)
-
-        // add the SVG element
-        var svg = d3.select("#barChart2").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform",
-                "translate(" + margin.left + "," + margin.top + ")");
-                data.forEach(function(d) {                 
-                    d.value = +d.value;
-                });
-                
-              // scale the range of the data
-              x.domain(data.map(function(d) { return d.key; }));
-              y.domain([0, d3.max(data, function(d) { return d.value; })]);
-            
-              // add axis
-              svg.append("g")
-                  .attr("class", "x axis")
-                  .attr("transform", "translate(0," + height + ")")
-                  .call(xAxis)
-                .selectAll("text")
-                  .style("text-anchor", "end")
-                  .attr("dx", "-.8em")
-                  .attr("dy", "-.55em")
-                  .attr("transform", "rotate(-90)" );
-            
-              svg.append("g")
-                  .attr("class", "y axis")
-                  .call(yAxis)
-                .append("text")
-                  .attr("transform", "rotate(-90)")
-                  .attr("y", 5)
-                  .attr("dy", ".71em")
-                  .style("text-anchor", "end")
-                  .text("Frequency");
-            
-            
-              // Add bar chart
-              svg.selectAll("bar")
-                  .data(data)
-                .enter().append("rect")
-                  .attr("class", "bar")
-                  .style("fill", function(d, i){
-                    return color(i);
-                   })
-                  .attr("x", function(d) { return x(d.key); })
-                  .attr("width", x.bandwidth() -1 )
-                  .attr("y", function(d) { return y(d.value); })
-                  .attr("height", function(d) { return height - y(d.value); })
-                  .text(function(d, i) {
-                    return data[i].label;
-                  });;
-
+        const width = 600,
+        height = 300,
+        margin = {
+          top: 20,
+          left: 100,
+          right: 40,
+          bottom: 40
+        };
+      
+      // Now, we don't use 0 as a minimum, but get it from the data using d3.extent
+      const x = d3.scaleLinear()
+        .domain(d3.extent(data.map(d => d.value)))
+        .range([0, width]);
+      
+      const y = d3.scaleBand()
+        .domain(data.map(d => d.key))
+        .range([height, 0])
+        .padding(0.1);
+      
+      const svg = d3.select("#barChart2").append("svg")
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom);
+      
+      const g = svg
+        .append('g')
+        .attr('transform', `translate(${margin.left} ${margin.right})`);
+      
+      const bars = g.append('g')
+        .selectAll('rect')
+        .data(data);
+      
+      bars.exit().remove();
+      
+      bars.enter()
+        .append('rect')
+        .merge(bars)
+        // All the same until here
+        // Now, if a bar is positive it starts at x = 0, and has positive width
+        // If a bar is negative it starts at x < 0 and ends at x = 0
+        .attr('x', d => d.value > 0 ? x(0) : x(d.value))
+        .attr('y', d => y(d.key))
+        // If the bar is positive it ends at x = v, but that means it's x(v) - x(0) wide
+        // If the bar is negative it ends at x = 0, but that means it's x(0) - x(v) wide
+        .attr('width', d => d.value > 0 ? x(d.value) - x(0) : x(0) - x(d.value))
+        .attr('height', y.bandwidth())
+        // Let's color the bar based on whether the value is positive or negative
+        .attr('fill', d => d.value > 0 ? 'darkgreen' : 'darkred')
+      
+      g.append('g')
+        .classed('x-axis', true)
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(x))
+      
+      g.append('g')
+        .classed('y-axis', true)
+        .call(d3.axisLeft(y))
 
 
     }, [])
